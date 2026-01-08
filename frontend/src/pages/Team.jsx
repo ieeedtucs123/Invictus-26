e3a2fd828bf4abc72c2d6026f17971d991234a7f
Zahanboi <zahansharma123@gmail.com>
2026-01-08 21:38:31 +0530

    temple work started

---
+++ frontend/src/pages/Team.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import TeamComponent from "@/components/Team/Team"
+
+export default function Team() {
+  return (
+    <TeamComponent/>
+  )
+}
---
+++ frontend/src/pages/model.jsx
@@ -0,0 +1,9 @@
+import ThreeScene from "@/components/Landing/Three/ThreeScene";
+
+export default function Home() {
+  return (
+    <main>
+      <ThreeScene />
+    </main>
+  );
+}
