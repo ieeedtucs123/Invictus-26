88873534d55a21c5b5bc0d84f5c36dd16edcfe18
Zahanboi <zahansharma123@gmail.com>
2026-01-10 01:49:46 +0530

    updates

--- frontend/src/components/Events/CardComponent.jsx
+++ frontend/src/components/Events/CardComponent.jsx
@@ -6,7 +6,7 @@ import {
   CarouselItem,
   CarouselNext,
   CarouselPrevious,
-} from '@/components/ui/carousel'
+} from '@/components/Events/ui/carousel'
 
 // --- 1. Event Card ---
 const EventCard = ({ title, isActive }) => {
