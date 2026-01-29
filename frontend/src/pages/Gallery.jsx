28633924b027ac75247c955850c6c0b960ea7136
Zahanboi <zahansharma123@gmail.com>
2026-01-29 17:20:25 +0530

    added non found page

--- frontend/src/pages/Gallery.jsx
+++ frontend/src/pages/Gallery.jsx
@@ -1,7 +1,7 @@
 import GalleryComponent from "@/components/Gallery/Gallery";
 
 export default function Gallery({setLotusClass, setLotusStyle, setFigureClass, setFigureStyle}) {
-  throw new Error("test crash");
+  // throw new Error("test crash");
 
   return (
     <GalleryComponent setLotusClass={setLotusClass} setLotusStyle={setLotusStyle} setFigureClass={setFigureClass} setFigureStyle={setFigureStyle} />
--- frontend/src/pages/_document.js
+++ frontend/src/pages/_document.js
@@ -13,7 +13,7 @@ export default function Document() {
           rel="stylesheet"
         />
         <link rel="preload" href="/intro/startVideo.mp4" as="video" />
-        <link rel="icon" href="/invictuslogo.svg" type="image/x-icon" />
+        <link rel="icon" href="/invictuslogo.png" type="image/x-icon" />
         <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
